586f8496f315833042a8ec5b49b2b3853b6bd6d7
mercuriussss <caijianlong615@outlook.com>
2018-09-12 22:35:21 +0800

    完工

--- src/wc/Count.java
+++ src/wc/Count.java
@@ -1,8 +1,6 @@
 package wc;
 
 import com.mysql.jdbc.StringUtils;
-
-import javax.sound.midi.Soundbank;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
--- src/wc/Main.java
+++ src/wc/Main.java
@@ -9,26 +9,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
 
-        System.out.println("\n\t 输入 “-c 文件名” 表示查询文件的字符数 ");
-        System.out.println("\n\t 输入 “-w 文件名” 表示查询文件的词的数目");
-        System.out.println("\n\t 输入 “-l 文件名” 表示查询文件的行数");
-        System.out.println("\n\t 输入 “-s 文件名” 表示查询文件的字符数");
+        System.out.println("\n\t 输入 “-c 文件完整路径” 表示查询文件的字符数 ");
+        System.out.println("\n\t 输入 “-w 文件完整路径” 表示查询文件的词的数目");
+        System.out.println("\n\t 输入 “-l 文件完整路径” 表示查询文件的行数");
+        System.out.println("\n\t 输入 “-a 文件完整路径” 表示查询文件的空行、注释行、代码行数");
+        System.out.println("\n\t 输入 “-s 其他功能指令 文件完整路径” 表示以特定功能查询文件夹下所有代码文件");
         System.out.println("\n\t 输入 “-e” 表示退出程序");
+        System.out.println("\n\t 以上除了退出功能外，其余皆可叠加指令查询，如-c -w -l 文件完整路径");
         while(true){
             System.out.println("\n请输入相应命令：");
             Scanner sc = new Scanner(System.in);
             //按空格切割成字符串数组，并将其分为指令组和文件路径
             String commands[] = sc.nextLine().toString().split("\\s");
+            //判断是否为退出指令
+            if(commands[0].equals("-e")){
+                break;
+            }
+
             String cms[] = Arrays.copyOf(commands,commands.length-1);
             String filePath = commands[commands.length-1];
             File file = new File(filePath);
 
-            //退出
-            if(cms[0].equals("-e")){
-                break;
-            }
+
             //递归查询
-            else if(cms[0].equals("-s")) {
+            if(cms[0].equals("-s")) {
                 //检验输入指令除了‘-s’外，还含有其他功能指令
                 if(cms.length>1){
                     String newcms[] = Arrays.copyOfRange(cms,1,cms.length);
